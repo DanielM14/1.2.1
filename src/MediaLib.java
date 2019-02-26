925cd6104c4361370a34d242062db83cdd049862
Daniel McGlasson <dmcglasson90@gmail.com>
2019-02-26 11:11:52 -0600

    update

--- src/MediaLib.java
+++ src/MediaLib.java
@@ -6,7 +6,7 @@
  */
 public class MediaLib
 {
-    public static void main()
+    public static void main(String args[])
     {
         double totalCost = 0.0;
         double totalRating = 0;
@@ -20,51 +20,72 @@ public class MediaLib
         numSongs = numSongs + 1;
         totalCost = totalCost + song1.getPrice();
         totalRating = totalRating + song1.getRating();
+        String t = song1.getTitle();
+        MediaFile.writeString(song1.getTitle()+ "|" + song1.getRating());
+
 
         Song song2 =  new Song("Smooth", .99, 9);
         numSongs = numSongs + 1;
         totalCost = totalCost + song2.getPrice();
         totalRating = totalRating + song2.getRating();
+        MediaFile.writeString(song2.getTitle()+ "|" + song2.getRating());
+
 
         Song song3 =  new Song("Mack the Knife", 1.29, 8);
         numSongs = numSongs + 1;
         totalCost = totalCost + song3.getPrice();       
         totalRating = totalRating + song3.getRating();
+        MediaFile.writeString(song3.getTitle()+ "|" + song3.getRating());
+
 
         Song song4 =  new Song("How Do I Live", 99, 7);
         numSongs = numSongs + 1;
         totalCost = totalCost + song4.getPrice();
         totalRating = totalRating + song4.getRating();
+        MediaFile.writeString(song4.getTitle()+ "|" + song4.getRating());
+
 
         Song song5 =  new Song("Party Rock Anthem", 1.29, 6);
         numSongs = numSongs + 1;
         totalCost = totalCost + song5.getPrice();
         totalRating = totalRating + song5.getRating();
+        MediaFile.writeString(song5.getTitle()+ "|" + song5.getRating());
+
 
         Song song6 =  new Song("I Gotta Feeling", .99, 7);
         numSongs = numSongs + 1;
         totalCost = totalCost + song6.getPrice();
         totalRating = totalRating + song6.getRating();
+        MediaFile.writeString(song6.getTitle()+ "|" + song6.getRating());
+
 
         Song song7 =  new Song("Macaren", 1.29, 8);
         numSongs = numSongs + 1;
         totalCost = totalCost + song7.getPrice();
         totalRating = totalRating + song7.getRating();
+        MediaFile.writeString(song7.getTitle()+ "|" + song7.getRating());
+
 
         Song song8 =  new Song("Physical", .99, 9);
         numSongs = numSongs + 1;
         totalCost = totalCost + song8.getPrice();
         totalRating = totalRating + song8.getRating();
+        MediaFile.writeString(song8.getTitle()+ "|" + song8.getRating());
+
 
         Song song9 =  new Song("You Light Up My Life", 1.29, 10);
         numSongs = numSongs + 1;
         totalCost = totalCost + song9.getPrice();
         totalRating = totalRating + song9.getRating();
+        MediaFile.writeString(song9.getTitle()+ "|" + song9.getRating());
 
         Song song10 =  new Song("Hey Jude", .99, 9);
         numSongs = numSongs + 1;
         totalCost = totalCost + song10.getPrice();
         totalRating = totalRating + song10.getRating();
+        MediaFile.writeString(song10.getTitle()+ "|" + song10.getRating());
+        MediaFile.saveAndClose();
+
 
         /**** test code is commented out
         double testVal = 109.41;
